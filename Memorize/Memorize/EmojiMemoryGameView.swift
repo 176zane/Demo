f410a5b5e907bb59de076a6f243828df4562a802
ybw <176zane@gmail.com>
2026-01-30 17:00:01 +0800

    .

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -15,21 +15,25 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView{
                cards
+                    .animation(.default, value: viewModel.cards)
             }
             .foregroundColor(.orange)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
         }
-        
-
         .padding()
     }
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 95),spacing: 0)],spacing: 0, content: {
-            ForEach(viewModel.cards.indices,id: \.self) { index in
-                CardView(card: viewModel.cards[index])
-                    .aspectRatio(2/3, contentMode: .fit).padding(4)
+            ForEach(viewModel.cards) { c in
+                CardView(card: c)
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(c)
+                    }
+                    
             }
         })
     }
@@ -42,18 +46,14 @@ struct CardView: View {
     var body: some View {
         ZStack(alignment: .center) {
             let base = RoundedRectangle(cornerRadius: 12)
-            
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(card.content).font(.system(size: 200)).minimumScaleFactor(0.01).aspectRatio(contentMode: .fit)
             }.opacity(card.isFaceUp ? 1 : 0)
-            
             base.fill().opacity(card.isFaceUp ? 0 : 1)
-            
-        }.onTapGesture {
-            
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 #Preview {
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
-struct MemoryGamge<CardContent>{
-    struct Card {
-        var isFaceUp: Bool = true
+struct MemoryGamge<CardContent> where CardContent:Equatable{
+    struct Card :Equatable,Identifiable{
+        var id: String
+        
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
     }
@@ -18,16 +20,49 @@ struct MemoryGamge<CardContent>{
         cards = []
         for i in 0..<numberOfPairsOfCards {
             let content = cardContentFactoty(i)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(id: "\(i)a", content: content))
+            cards.append(Card(id: "\(i)b", content: content))
         }
     }
-    
-    func choose(card:Card) {
-        
+    var theOneAndOnlyFaceUpCardIndex: Int? {
+        get {
+            cards.indices.filter{cards[$0].isFaceUp}.only
+        }
+        set {
+            cards.indices.forEach { i in
+                cards[i].isFaceUp = i == newValue
+            }
+        }
+    }
+    mutating func choose(card:Card) {
+        if let i = cards.firstIndex(where: {$0 == card}) {
+           
+            if !cards[i].isFaceUp && !cards[i].isMatched {
+                if let oneIndex = theOneAndOnlyFaceUpCardIndex {
+                    if cards[i].content == cards[oneIndex].content {
+                        cards[i].isMatched = true
+                        cards[oneIndex].isMatched = true
+                    }
+                }else {
+                    theOneAndOnlyFaceUpCardIndex = i
+                }
+                cards[i].isFaceUp = true
+            }
+        }
+
+//        //错误用法，cards.first返回的是第一个满足条件的元素的副本，并不是元素本身，此处值类型修改属性对数组里面的元素本身无效
+//        if var c = cards.first(where: {$0 == card}) {
+//            c.isFaceUp.toggle()
+//        }
+       
     }
     mutating func shuffle(){
         cards.shuffle()
     }
-    
+}
+
+extension Array{
+    var only: Element? {
+        count == 1 ? first : nil
+    }
 }
