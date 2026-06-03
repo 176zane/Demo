c98b4e0f60c3b864a9e054564336f8a1b6f61062
ybw <176zane@gmail.com>
2026-06-03 16:52:56 +0800

    Working with UI controls

---
+++ LandMarks/LandMarks/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  LandMarks
+//
+//  Created by ybw on 2026/4/30.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
